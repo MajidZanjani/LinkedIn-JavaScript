a393292d951db4161135f74223df85db39827269
MajidZanjani <majidmmz1972@gmail.com>
2023-10-01 18:05:05 +0400

    commit

--- JS Essential Training/08_15/script.js
+++ JS Essential Training/08_15/script.js
@@ -3,16 +3,23 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
  */
 
-const stuff = ["piggy", "headlamp", "pen", "pencil", "eraser", "water bottle"];
+const stuff = ['piggy', 'headlamp', 'pen', 'pencil', 'eraser', 'water bottle'];
 
-const article = document.querySelector("article");
-let stuffList = document.createElement("ul");
+const article = document.querySelector('article');
+let stuffList = document.createElement('ul');
 
 // forEach() array method
-stuff.forEach((item) => {
-  let listItem = document.createElement("li");
+const stuffItems = stuff.map((item) => {
+  let listItem = document.createElement('li');
   listItem.innerHTML = item;
-  stuffList.append(listItem);
+  return listItem;
 });
 
-article.append(stuffList)
+stuffItems.forEach((item) => {
+  stuffList.append(item);
+});
+
+console.log('stuff: ', stuff);
+console.log('stuffItems: ', stuffItems);
+
+article.append(stuffList);
