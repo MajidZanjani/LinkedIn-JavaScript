ec39b97521bd9506965d83e5d488a10c0d0fd221
MajidZanjani <majidmmz1972@gmail.com>
2023-10-08 16:11:57 +0400

    commit

---
+++ JS Essential Training/Practice/eventListener/script.js
@@ -0,0 +1,25 @@
+function updatePage() {
+  // Your code goes here.
+  const forms = document.querySelectorAll('form');
+  forms.forEach((form) => {
+    form.addEventListener('submit', (ev) => {
+      ev.preventDefault();
+      const target = ev.target.getAttribute('data-target');
+      const inputEl = document.getElementById(`${target}-input`);
+      const targetEl = document.getElementById(target).querySelector('.value');
+      targetEl.textContent = inputEl.value;
+    });
+  });
+}
+
+const inputs = [
+  { id: 'name-input', value: 'Adventure Backpack' },
+  {
+    id: 'description-input',
+    value: 'A durable backpack for outdoor adventures',
+  },
+  { id: 'color-input', value: 'Green' },
+  { id: 'volume-input', value: '35L' },
+];
+
+updatePage();
